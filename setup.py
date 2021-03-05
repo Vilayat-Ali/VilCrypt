b67bfeefc58aa5add1fab246d297421fa055ec7f
Syed Vilayat Ali Rizvi <vilayatcodemysite@gmail.com>
2021-03-05 05:21:11 -0800

    setup.py added

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+
+from setuptools import setup
+from setuptools import find_namespace_packages
+from setuptools import find_packages
+
+setup(
+    name='VilCrypt',
+    version='1.0.0',
+    packages=['Data'],
+    package_dir={'':'Data'},
+    license='',
+    author='Syed Vilayat Ali Rizvi',
+    author_email='vilayatcodemysite@gmail.com',
+    description='The ultimate encryption program...'
+
+)
